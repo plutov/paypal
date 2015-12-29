74d2d9dcde008cfe42fea05ad18b638a9eae1116
Aliaksandr Pliutau <pliutau.aliaksandr@lazada.com>
2015-12-29 13:02:03 +0700

    godoc

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 [![Build Status](https://travis-ci.org/logpacker/paypalsdk.svg?branch=master)](https://travis-ci.org/logpacker/paypalsdk)
+[![Godoc](http://img.shields.io/badge/godoc-reference-blue.svg?style=flat)](https://godoc.org/github.com/logpacker/paypalsdk)
 
 #### GO client for PayPal REST API
 
---
+++ doc.go
@@ -0,0 +1,10 @@
+/*
+Package paypalsdk provides a warepper to PayPal API (https://developer.paypal.com/webapps/developer/docs/api/).
+The first thing you do is to create a Client.
+  c, err := paypalsdk.NewClient("clientID", "secretID", paypalsdk.APIBaseSandBox)
+Then you can get an access token from PayPal:
+  accessToken, err := c.GetAccessToken()
+After you have an access token you can call built-in funtions to get data from PayPal.
+paypalsdk will assign all responses to go structures.
+*/
+package paypalsdk
