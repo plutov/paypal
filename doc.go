8ad0c9375866392412052af83bec360dff80c320
Aliaksandr Pliutau <pliutau.aliaksandr@lazada.com>
2016-03-07 13:00:22 +0700

    GoDoc example for CreatePayment

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 /*
 Package paypalsdk provides a warepper to PayPal API (https://developer.paypal.com/webapps/developer/docs/api/).
-The first thing you do is to create a Client (you can select API base URL using paypalsdk contsnts).
+The first thing you do is to create a Client (you can select API base URL using paypalsdk contants).
   c, err := paypalsdk.NewClient("clientID", "secretID", paypalsdk.APIBaseSandBox)
 Then you can get an access token from PayPal:
   accessToken, err := c.GetAccessToken()
---
+++ doc_test.go
@@ -0,0 +1,51 @@
+package paypalsdk_test
+
+import "github.com/logpacker/PayPal-Go-SDK"
+
+func ExampleCreatePayment() {
+	// Initialize client
+	c, err := paypalsdk.NewClient("clientID", "secretID", paypalsdk.APIBaseSandBox)
+	if err != nil {
+		panic(err)
+	}
+
+	// Retreive access token
+	_, err = c.GetAccessToken()
+	if err != nil {
+		panic(err)
+	}
+
+	// Create credit card payment
+	p := paypalsdk.Payment{
+		Intent: "sale",
+		Payer: &paypalsdk.Payer{
+			PaymentMethod: "credit_card",
+			FundingInstruments: []paypalsdk.FundingInstrument{paypalsdk.FundingInstrument{
+				CreditCard: &paypalsdk.CreditCard{
+					Number:      "4111111111111111",
+					Type:        "visa",
+					ExpireMonth: "11",
+					ExpireYear:  "2020",
+					CVV2:        "777",
+					FirstName:   "John",
+					LastName:    "Doe",
+				},
+			}},
+		},
+		Transactions: []paypalsdk.Transaction{paypalsdk.Transaction{
+			Amount: &paypalsdk.Amount{
+				Currency: "USD",
+				Total:    "7.00",
+			},
+			Description: "My Payment",
+		}},
+		RedirectURLs: &paypalsdk.RedirectURLs{
+			ReturnURL: "http://...",
+			CancelURL: "http://...",
+		},
+	}
+	_, err = c.CreatePayment(p)
+	if err != nil {
+		panic(err)
+	}
+}
