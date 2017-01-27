13492e99b992b4c0dd2ef6f340eba4adcc019d15
Jonathan Weber <bejonwe@gmx.net>
2017-01-27 15:00:29 +0100

    change type of Item.Amount from string to int to get it working with the newest PayPal API

--- types.go
+++ types.go
@@ -193,7 +193,7 @@ type (
 
 	// Item struct
 	Item struct {
-		Quantity    string `json:"quantity"`
+		Quantity    int    `json:"quantity"`
 		Name        string `json:"name"`
 		Price       string `json:"price"`
 		Currency    string `json:"currency"`
