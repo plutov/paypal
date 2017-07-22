f12db629955bc0d07bcb72d765cb99b08b8cc8ab
envy124 <aspidfire9664@gmail.com>
2017-07-22 14:48:16 +0300

    PaymentDefinition add ID field

--- types.go
+++ types.go
@@ -290,6 +290,7 @@ type (
 
 	// PaymentDefinition struct
 	PaymentDefinition struct {
+		ID                string        `json:"id,omitempty"`
 		Name              string        `json:"name,omitempty"`
 		Type              string        `json:"type,omitempty"`
 		Frequency         string        `json:"frequency,omitempty"`
