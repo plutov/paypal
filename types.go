5822d5ee5849c28bc2a0cf176bbf8430e8c6c922
Eric Lee <eric.lee@bolt.com>
2019-12-16 16:34:48 -0800

    Print error details in addition to message and http code (#131)
    * Print error details in addition to message and http code
    
    * %v -> %+v

--- types.go
+++ types.go
@@ -978,7 +978,7 @@ type (
 
 // Error method implementation for ErrorResponse struct
 func (r *ErrorResponse) Error() string {
-	return fmt.Sprintf("%v %v: %d %s", r.Response.Request.Method, r.Response.Request.URL, r.Response.StatusCode, r.Message)
+	return fmt.Sprintf("%v %v: %d %s, %+v", r.Response.Request.Method, r.Response.Request.URL, r.Response.StatusCode, r.Message, r.Details)
 }
 
 // MarshalJSON for JSONTime
