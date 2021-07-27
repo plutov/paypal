c2074af7362290417b33ed9e38be009ad319baf7
Cameron Jarnot <48696097+cameronjarnot1@users.noreply.github.com>
2021-07-27 14:59:36 -0400

    Update payment source type for optional fields (#216)
    * updated payment source fields
    
    * revert
    
    * update payment source type

--- types.go
+++ types.go
@@ -801,8 +801,8 @@ type (
 
 	// PaymentSource structure
 	PaymentSource struct {
-		Card  *PaymentSourceCard  `json:"card"`
-		Token *PaymentSourceToken `json:"token"`
+		Card  *PaymentSourceCard  `json:"card,omitempty"`
+		Token *PaymentSourceToken `json:"token,omitempty"`
 	}
 
 	// PaymentSourceCard structure
