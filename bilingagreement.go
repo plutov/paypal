4ba746e0cf691a1e7db2b0574714f9168898382e
zoranmiladinoski <zoki_m@live.com>
2022-06-17 13:19:16 +0200

    Added new operation CancelAgreement

--- bilingagreement.go
+++ bilingagreement.go
@@ -13,7 +13,7 @@ type AgreementRequest struct {
 // Endpoint: GET /v1/payments/billing-agreements/subID/suspend"
 func (c *Client) SuspendAgreement(bAID string, agr AgreementRequest) (*DefaultResponse, error) {
 
-	req, err := c.NewRequest("POST", fmt.Sprintf("%s%s", c.APIBase, "/v1/payments/billing-agreements/"+bAID+"/suspend", ), agr)
+	req, err := c.NewRequest("POST", fmt.Sprintf("%s%s", c.APIBase, "/v1/payments/billing-agreements/"+bAID+"/suspend"), agr)
 	resp := &DefaultResponse{}
 
 	if err != nil {
@@ -29,7 +29,23 @@ func (c *Client) SuspendAgreement(bAID string, agr AgreementRequest) (*DefaultRe
 // Endpoint: GET /v1/payments/billing-agreements/subID/suspend"
 func (c *Client) ReActivateAgreement(bAID string, agr AgreementRequest) (*DefaultResponse, error) {
 
-	req, err := c.NewRequest("POST", fmt.Sprintf("%s%s", c.APIBase, "/v1/payments/billing-agreements/"+bAID+"/re-activate", ), agr)
+	req, err := c.NewRequest("POST", fmt.Sprintf("%s%s", c.APIBase, "/v1/payments/billing-agreements/"+bAID+"/re-activate"), agr)
+	resp := &DefaultResponse{}
+
+	if err != nil {
+		return resp, err
+	}
+
+	err = c.SendWithBasicAuth(req, resp)
+	return resp, err
+}
+
+// CancelAgreement returns status code and message
+// cancel billing-agreement
+// Endpoint: GET /v1/payments/billing-agreements/subID/cancel"
+func (c *Client) CancelAgreement(bAID string, agr AgreementRequest) (*DefaultResponse, error) {
+
+	req, err := c.NewRequest("POST", fmt.Sprintf("%s%s", c.APIBase, "/v1/payments/billing-agreements/"+bAID+"/cancel"), agr)
 	resp := &DefaultResponse{}
 
 	if err != nil {
